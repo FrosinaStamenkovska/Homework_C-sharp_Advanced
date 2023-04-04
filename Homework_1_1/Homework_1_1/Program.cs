75c5f0a36b4df9db46b94dc1abae7883abfad3d5
FrosinaStamenkovska <117694114+FrosinaStamenkovska@users.noreply.github.com>
2023-04-04 10:15:18 +0200

    Add files via upload

---
+++ Homework_1_1/Homework_1_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Homework_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            List <string> listOfNames = new List <string> ();
+            while(true)
+            {
+                Console.WriteLine("Please enter a name or press x to exit!");
+                string input = Console.ReadLine();
+                if(input.ToLower() == "x")
+                {
+                    break;
+                }
+
+                if(!Regex.IsMatch(input, @"^[a-zA-Z]+$") || input.Length < 2)
+                {
+                    Console.WriteLine("Invalid name!");
+                    continue;
+                }
+
+                listOfNames.Add(input);
+            }
+
+            Console.WriteLine("Please enter a text");
+            string someText = Console.ReadLine();
+
+            foreach(string name in listOfNames)
+            {
+                Console.WriteLine($"The name {name} was included in the text {Regex.Matches(someText.ToLower(),name.ToLower()).Count()} times");
+            }
+
+        }
+
+        
+
+        
+    }
+}
